c79fde162e6756cb01619422cbebe474c870afe2
sreenisha123 <119654784+sreenisha123@users.noreply.github.com>
2022-12-02 12:54:51 +0530

    Add files via upload

---
+++ EmployeeSkillApplication.java
@@ -0,0 +1,13 @@
+package com.employeeskills;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeSkillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeSkillApplication.class, args);
+    }
+
+}
