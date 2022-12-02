829953292f3a68f6ce0d7cc390070023dbf49f39
sreenisha123 <119654784+sreenisha123@users.noreply.github.com>
2022-12-02 12:52:14 +0530

    Add files via upload

---
+++ ApplicationConstants.java
@@ -0,0 +1,6 @@
+package com.employeeskills.constants;
+
+public class ApplicationConstants {
+    public static final String EMP_LIST_SCREEN = "employeeList";
+    public static final String EMP_FORM_SCREEN = "employee-form";
+}
